9cde1e07934dba29a059d918a50921a03147bc97
Pavel Beschetnov <geomslayer@gmail.com>
2017-08-13 19:00:16 +0300

    Create VKAuth class and load friends list

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from vk_login import VKAuth
+
+APP_ID = '6087891'
+PERMISSIONS = ['friends']
+API_VERSION = '5.67'
+
+auth_object = VKAuth(PERMISSIONS, APP_ID, API_VERSION)
+auth_object.auth()
+r = requests.get('https://api.vk.com/method/friends.get',
+                 params={'access_token': auth_object.get_token(), 'v': API_VERSION})
+print(r.json())

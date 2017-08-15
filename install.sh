26a1d96581d9b088e6044855181a71c3e0517329
Pavel Beschetnov <geomslayer@gmail.com>
2017-08-15 14:05:40 +0300

    Add requirements and install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O ~/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xjvf ~/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C ~/Downloads
+sudo mv ~/Downloads/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
